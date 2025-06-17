d6b98676cc2356efe56d7813b6a8fe485f678840
Alan <albou@netc.fr>
2025-06-17 17:12:38 +0200

    debugging port

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173, // Ceci est le port de développement
+    port: 9000, // Ceci est le port de développement
   },
   preview: {
     port: 9000, // Ceci sera le port utilisé par `vite preview`
